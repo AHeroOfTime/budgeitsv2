a916fe412ea865f35530936b818f8d5033930fc5
Aaron Rodriguez <aaronerdev@gmail.com>
2025-08-05 13:26:45 -0500

    added reset functionality to header button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,22 @@
+<script>
+	import { monthlyIncome, fixedSpendingList, variableSpendingList } from '../lib/store.svelte';
+
+	function resetEverything() {
+		if (confirm('Are you sure you want to reset all of your information?')) {
+			monthlyIncome.current = 0;
+			fixedSpendingList.current.length = 0;
+			variableSpendingList.current.length = 0;
+		} else {
+			return;
+		}
+	}
+</script>
+
 <nav>
 	<ul>
 		<li><strong>Budge It</strong></li>
 	</ul>
 	<ul>
-		<li><button class="secondary">Reset</button></li>
+		<li><button onclick={resetEverything} class="secondary">Reset</button></li>
 	</ul>
 </nav>
